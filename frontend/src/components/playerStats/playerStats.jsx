7ba98b5bbcb406fc80152b2a464bbde4c2338b00
Calvin Mozola <calvin@stuntfactory.com>
2022-06-21 14:26:18 -0400

    player stats bar

---
+++ frontend/src/components/playerStats/playerStats.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+import Grid from '@mui/material/Grid';
+import { Line, Circle } from 'rc-progress';
+import Styling from './styling.module.css'
+
+
+const PlayerStats = ({health,maxHealth,money,mana,maxMana,name}) => {
+
+    return(<Grid
+  container
+  direction="row"
+  justifyContent="space-between"
+  alignItems="flex-start"
+  spacing={2}
+  className={Styling['holder']}
+>
+        <Grid item sm={6} xs={12}>
+          <div>
+              <div>{name}'s info</div>
+              <div>Gold:{money}</div>
+              </div>
+        </Grid>
+        <Grid item sm={6} xs={12}>
+
+        <Grid
+  container
+  direction="row"
+  justifyContent="flex-end"
+>
+          <Grid item >
+            <div className={Styling['circle-holder']}>
+          <Circle className={Styling['circle']} percent={health/maxHealth * 100} strokeWidth={4} strokeColor="#ff0d00" /> 
+          </div>
+          </Grid>
+          <Grid  item className={Styling['circle-holder']}>
+          <div className={Styling['circle-holder']}>
+          <Circle className={Styling['circle']} percent={mana/maxMana * 100} strokeWidth={4} strokeColor="#4269f5" /> 
+          </div>
+          </Grid>
+          
+</Grid>
+
+        </Grid>
+      </Grid>)
+}
+
+export default PlayerStats
