1973e670da08dc98b7fca3d442fd93506492be18
Blake Gigiolio <bgigiolio@gmail.com>
2022-06-22 16:20:12 -0400

    added hp and mana counters

--- frontend/src/components/playerStats/playerStats.jsx
+++ frontend/src/components/playerStats/playerStats.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Grid from '@mui/material/Grid';
 import { Line, Circle } from 'rc-progress';
 import Styling from './styling.module.css'
@@ -7,6 +7,17 @@ import LinearProgress from '@mui/material/LinearProgress';
 
 
 const PlayerStats = ({ health, maxHealth, money, mana, maxMana, name, experience, experienceToNextLevel, level }) => {
+  let rect = {left: 0, right: 0, top: 0, bottom: 0}
+  let rectMa = {left: 0, right: 0, top: 0, bottom: 0}
+  useEffect(()=>{
+    const el = document.getElementById("healthCircle")
+    rect = el.getBoundingClientRect();
+
+    const ma = document.getElementById("manaCircle")
+    rectMa = ma.getBoundingClientRect();
+
+    console.log(rect.top, rect.right, rect.bottom, rect.left);
+  })
 
   return (<Grid
     container
@@ -34,12 +45,14 @@ const PlayerStats = ({ health, maxHealth, money, mana, maxMana, name, experience
       >
         <Grid item lg={12} xs={6}>
           <div className={Styling['circle-holder']}>
-            <Circle className={Styling['circle']} percent={health / maxHealth * 100} strokeWidth={4} strokeColor="#ff0d00" />
-          </div>
+            <p id="hp" style={{marginTop: rect.top + 40, marginBottom: rect.bottom, marginLeft: rect.left + 58, marginRight: rect.right}}>{health}</p>
+            <Circle id="healthCircle" className={Styling['circle']} percent={health / maxHealth * 100} strokeWidth={4} strokeColor="#ff0d00" />
+          </div> 
         </Grid>
         <Grid item lg={12} xs={6} className={Styling['circle-holder']}>
           <div className={Styling['circle-holder']}>
-            <Circle className={Styling['circle']} percent={mana / maxMana * 100} strokeWidth={4} strokeColor="#4269f5" />
+          <p id="mana" style={{marginTop: rectMa.top + 40, marginBottom: rectMa.bottom, marginLeft: rectMa.left + 58, marginRight: rectMa.right}}>{mana}</p>
+            <Circle id="manaCircle" className={Styling['circle']} percent={mana / maxMana * 100} strokeWidth={4} strokeColor="#4269f5" />
           </div>
         </Grid>
 
